46319dcf88de975614e84375a328f852ae8cbd97
Vijay-Patidar-2226 <vijayharniya6267@gmail.com>
2024-11-29 01:45:21 -0800

    Add files via upload

---
+++ FIRST.C
@@ -0,0 +1,67 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("Jai Ganesh\n");
+printf("Jay shree ram\n");
+printf("Jay ram\n");
+printf("Hello\n");
+getch();
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
